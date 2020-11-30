526e3280970a826ab3b6ce4a137c9ba216c37460
johannesjh <43996-johannesjh@users.noreply.gitlab.gnome.org>
2020-11-30 21:58:24 +0100

    Working Webview

--- fava_desktop/main.py
+++ fava_desktop/main.py
@@ -1,10 +1,18 @@
 import gi
 
-gi.require_versions({"Gtk": "3.0"})
-from gi.repository import Gtk  # noqa: E402
-
+gi.require_versions({"Gtk": "3.0", "WebKit2": "4.0"})
+from gi.repository import Gtk, WebKit2 as WebKit  # noqa: E402
 
 window = Gtk.Window(title="Hello World")
-window.show()
+window.set_default_size(700, 500)
+
+webview = WebKit.WebView()
+webview.load_uri("https://gnome.org/")
+scrolled_window = Gtk.ScrolledWindow()
+scrolled_window.add(webview)
+window.add(scrolled_window)
+
+window.show_all()
 window.connect("destroy", Gtk.main_quit)
+
 Gtk.main()
