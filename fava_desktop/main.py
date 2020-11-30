28836c8a3f29b200640e04c720abc8e278ce9dd1
johannesjh <43996-johannesjh@users.noreply.gitlab.gnome.org>
2020-11-30 22:09:50 +0100

    Refactoring, now using classes

--- fava_desktop/main.py
+++ fava_desktop/main.py
@@ -1,18 +1,37 @@
+import sys
+
 import gi
 
 gi.require_versions({"Gtk": "3.0", "WebKit2": "4.0"})
 from gi.repository import Gtk, WebKit2 as WebKit  # noqa: E402
 
-window = Gtk.Window(title="Hello World")
-window.set_default_size(700, 500)
 
-webview = WebKit.WebView()
-webview.load_uri("https://gnome.org/")
-scrolled_window = Gtk.ScrolledWindow()
-scrolled_window.add(webview)
-window.add(scrolled_window)
+class Application(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self)
 
-window.show_all()
-window.connect("destroy", Gtk.main_quit)
+    def do_activate(self):
+        window = ApplicationWindow(self)
+        window.show_all()
 
-Gtk.main()
+
+class ApplicationWindow(Gtk.ApplicationWindow):
+    def __init__(self, application):
+        Gtk.Window.__init__(self, application=application)
+        self.set_title("Fava")
+        self.set_default_size(700, 500)
+
+        self.webview = WebKit.WebView()
+        self.webview.load_uri("https://gnome.org/")
+
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.add(self.webview)
+        self.add(scrolled_window)
+
+        self.connect("destroy", Gtk.main_quit)
+
+
+if __name__ == "__main__":
+    application = Application()
+    exit_status = application.run(sys.argv)
+    sys.exit(exit_status)
