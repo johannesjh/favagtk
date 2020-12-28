9bc4050ef2cb9dcfbaf522dd53245111365c2e04
johannesjh <johannesjh@users.noreply.github.com>
2020-12-28 21:16:59 +0100

    started working on unit tests

--- tests/test_fava_desktop.py
+++ tests/test_fava_desktop.py
@@ -1,5 +1,32 @@
-from fava_desktop import __version__
+import time
+import unittest
+from unittest import TestCase
+
+import gi
+
+from fava_desktop.main import ApplicationWindow
+
+gi.require_versions({"GdkPixbuf": "2.0", "Gdk": "3.0", "Gtk": "3.0", "WebKit2": "4.0"})
+from gi.repository import Gtk  # noqa: E402
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def refresh_gui(delay=0):
+    while Gtk.events_pending():
+        Gtk.main_iteration_do(blocking=False)
+    time.sleep(delay)
+
+
+class ApplicationWindowTest(TestCase):
+    def setUp(self):
+        self.window = ApplicationWindow(None)
+        self.window.connect("destroy", Gtk.main_quit)
+        self.window.show_all()
+        refresh_gui()
+
+    def test_initial_view(self):
+        """Assert that the placeholder view is initially shown."""
+        assert self.window.stack.get_visible_child() == self.window.placeholder_view
+
+
+if __name__ == "__main__":
+    unittest.main()
