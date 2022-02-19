fbd3112a11fb4e7765dffe4a4d3b4a43debb2ac0
johannesjh <43996-johannesjh@users.noreply.gitlab.gnome.org>
2022-02-19 16:32:15 +0100

    Publishes packages to pypi, using github workflows.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 
+from setuptools import find_packages
 from setuptools import setup
 
 with open(os.path.join("requirements", "main.in")) as req:
@@ -9,11 +10,37 @@ with open(os.path.join("requirements", "main.in")) as req:
 with open(os.path.join("requirements", "dev.in")) as req:
     TEST_REQUIRES = list(req)
 
+with open("README.md") as f:
+    README = f.read()
+
 
 # See setup.cfg for additional configuration.
 setup(
+    name="fava-gtk",
+    version="0.1.5-dev9",
+    license="GPL-2.0",
+    packages=find_packages(exclude="tests"),
+    include_package_data=True,
+    package_data={"": ["*.svg", "*.ui", "README.md"]},
+    python_requires=">=3.8",
     install_requires=INSTALL_REQUIRES,
     tests_require=TEST_REQUIRES,
-    include_package_data=True,
-    package_data={"": ["*.svg", "*.ui"]},
+    entry_points={
+        "console_scripts": [
+            "fava-gtk = fava_gtk.main:main",
+        ],
+    },
+    description="Use Fava and Beancount as a Desktop Application.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johannesjh/fava-gtk/",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: X11 Applications :: GTK",
+        "Environment :: X11 Applications :: Gnome",
+        "Topic :: Office/Business :: Financial :: Accounting",
+    ],
 )
