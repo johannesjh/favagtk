c6d7074dd0b421ff28a2b0932e4d09a3557e4a80
johannesjh <43996-johannesjh@users.noreply.gitlab.gnome.org>
2022-04-09 21:12:30 +0200

    updates python dependencies

--- flatpak-pip-update-modules.sh
+++ flatpak-pip-update-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PIP_GENERATOR="./flatpak-pip-generator --checker-data --runtime=org.gnome.Sdk//42"
+PIP_GENERATOR="./flatpak-pip-generator --checker-data --runtime=org.gnome.Sdk//master"
 $PIP_GENERATOR fava --output=python3-fava
 
