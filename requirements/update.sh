f17ef2a347798c191982d2a6e10fdec71cf9d666
johannesjh <43996-johannesjh@users.noreply.gitlab.gnome.org>
2022-06-21 02:35:01 +0200

    successful build from source packages

--- requirements/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# updates the json file with python dependencies for the flatpak build.
-
-echo "Generating json file..."
-./flatpak-pip-generator -r requirements.txt --runtime org.gnome.Sdk//master
-echo "Done generating json file."
